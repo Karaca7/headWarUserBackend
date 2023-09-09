f6836c10a50b94ed3112d55ec5a79cf931fc880e
Karaca7 <karacaylmaz2016@gmail.com>
2023-09-09 20:34:40 +0300

    feat(findmatch.js):finnish but connection close is problem

--- router/users/handlers/findMatch.js
+++ router/users/handlers/findMatch.js
@@ -1,23 +1,37 @@
 let clients = [];
 
-const colyseus = require('colyseus.js');
-const colyClient = new colyseus.Client('ws://colyseus_server_url'); 
+
+const Colyseus = require('colyseus.js');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const endpoint = 'ws://localhost:2567'; // Colyseus sunucusunun WebSocket adresi
+
+// Colyseus istemci bağlantısını oluşturun
+const client = new Colyseus.Client(endpoint);
 
 
 exports.handler = async (req, res) => {
   try {
+
+
+
     const headers = {
       "Content-Type": "text/event-stream",
       Connection: "keep-alive",
       "Cache-Control": "no-cache",
-      "Access-Control-Allow-Origin": "*",  //corsa takılmak hoşuna gitmez ekle kardeşim
+      "Access-Control-Allow-Origin": "*", //corsa takılmak hoşuna gitmez ekle kardeşim
     };
 
-
-    console.log(req.headers['authorization'].split(' ')[1]);
+    console.log(req.headers["authorization"].split(" ")[1]);
     res.raw.writeHead(200, headers);
 
     res.raw.write(`data: ${JSON.stringify("first")}\n\n`);
+
 
     const clientId = req.id;
 
@@ -25,39 +39,50 @@ exports.handler = async (req, res) => {
     const newClient = {
       id: clientId,
       response: res,
-      token:req.headers['authorization'].split(' ')[1],
-      count:0,
-      level:req.user.level,
-      
+      token: req.headers["authorization"].split(" ")[1],
+      count: 0,
+      level: req.user.level,
     };
 
     clients.push(newClient);
-    console.log(colyClient);
-
+    // console.log(colyClient);
 
     while (true) {
       for (const client of clients) {
-        client.count+=1;
+        client.count += 1;
 
-        // if(user match ) { client.create( )}
+        let matchedList = getMatchingClients(client);
 
+        console.log(matchedList);
 
+        if (matchedList.length > 1) {
+       
 
-        
-        if(client.count>=300 ) {
-          client.response.raw.write(`data: ${JSON.stringify("bye!")}\n\n`);
+          let newRoom= await createRoom()
+          console.log(newRoom.id,"rtesda");
+          client.response.raw.write(
+            `data: ${JSON.stringify(`{roomID:${newRoom.id}}`)}\n\n`
+          );
 
-          clients=clients.filter(el=>el.id!==client.id)
-          // console.log(clients,"new clients list");
-          return  await client.response.raw.close()
-
+          for (let cc of matchedList) {
+            console.log("flagg: ");
+            clients = clients.filter((el) => el.id !== el.id);
+            return  client.response.raw.close();
+          }
         }
 
-        console.log(client.count,"client count");
+        if (client.count >= 300) {
+          client.response.raw.write(`data: ${JSON.stringify("bye!")}\n\n`);
+
+          clients = clients.filter((el) => el.id !== client.id);
+          // console.log(clients,"new clients list");
+          return await client.response.raw.close();
+        }
+
+        // console.log(client.count,"client count");
         client.response.raw.write(`data: ${JSON.stringify(client.token)}\n\n`);
 
         await sleep(1000);
-
       }
 
       // console.log(clients);
@@ -69,6 +94,9 @@ exports.handler = async (req, res) => {
       clients = clients.filter((client) => client.id !== clientId);
     });
   } catch (error) {
+
+    console.log("eeror"
+    ,error);
     res.serverError("500");
   }
 };
@@ -76,8 +104,6 @@ exports.handler = async (req, res) => {
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
-
 
 function getMatchingClients(client) {
   const matchingClients = [];
@@ -87,10 +113,28 @@ function getMatchingClients(client) {
       const levelDiff = Math.abs(otherClient.level - client.level);
 
       if (levelDiff <= 5) {
-        matchingClients.push(otherClient);
+        matchingClients.push(otherClient, client);
       }
     }
   }
 
   return matchingClients;
 }
+
+
+
+async function createRoom() {
+  try {
+    // Sunucu tarafında tanımlanmış bir oda adıyla oda oluşturun
+    const room = await client.create('my_room');
+
+    return room
+    console.log('Oda oluşturuldu. Oda ID:', room.id);
+
+    
+  }
+
+catch(e){
+
+  console.log(e);
+}}
